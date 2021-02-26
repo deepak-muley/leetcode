15736c701dc47cdeaee42f1f001abaee9db1477e
deepak-muley <deepak.muley@gmail.com>
2021-02-25 22:43:13 -0800

    fix for allowing to return non-consecutive indexes

--- TwoSum/two_sum.py
+++ TwoSum/two_sum.py
@@ -3,29 +3,30 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mapTargetTo2IndicesList = {}
+        map = {}
         idx = 0
-        while idx < len(nums) - 1:
-            first = nums[idx]
-            second = nums[idx + 1]
-            sum = first + second
-            mapTargetTo2IndicesList[sum] = [idx, idx + 1]
+        while idx < len(nums):
+            complement = target - nums[idx]
+            if complement in map:
+                return [map[complement], idx]
+            map[nums[idx]] = idx
             idx += 1
-
-        if target in mapTargetTo2IndicesList:
-            return mapTargetTo2IndicesList[target]
 
 
 soln = Solution()
 
 nums = [2, 7, 11, 15]
 target = 9
-print("nums={} target={} indexes{}".format(nums, target, soln.twoSum(nums, target)))
+print("nums={} target={} indexes={}".format(nums, target, soln.twoSum(nums, target)))
 
 nums = [3, 2, 4]
 target = 6
-print("nums={} target={} indexes{}".format(nums, target, soln.twoSum(nums, target)))
+print("nums={} target={} indexes={}".format(nums, target, soln.twoSum(nums, target)))
 
 nums = [3, 3]
 target = 6
-print("nums={} target={} indexes{}".format(nums, target, soln.twoSum(nums, target)))
+print("nums={} target={} indexes={}".format(nums, target, soln.twoSum(nums, target)))
+
+nums = [3, 4, 9, 100]
+target = 12
+print("nums={} target={} indexes={}".format(nums, target, soln.twoSum(nums, target)))
